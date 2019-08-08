03d3356d7b7c1764f2d74bdb0b9e943effa821eb
David Forer <david.j.forer@gmail.com>
2019-08-08 05:03:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   attr_accessor :id, :name, :breed 
   
-  def intialize(:id = nil, :name, :breed)
+  def initialize(:id = nil, :name, :breed)
     @name = name
     @id = id
     @breed = breed
